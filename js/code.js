d69387d553cb02ec3e45315bf2b22b8ee0bcad8d
Alnwick62 <carette.romain62400@gmail.com>
2025-03-31 10:18:28 +0200

    modif

--- js/code.js
+++ js/code.js
@@ -19,6 +19,5 @@ window.onclick = function(event) {
 function submitForm(event) {
     event.preventDefault(); 
     document.getElementById("contact-form").submit();
-    alert("✅ Merci pour votre message ! Nous vous répondrons bientôt.");
     document.getElementById("contact-form").reset();
 }
